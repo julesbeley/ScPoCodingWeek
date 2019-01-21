dd0bb120090dbd43184fd25a6d0444d6e0055592
Jules Beley <jules.beley@gmail.com>
2019-01-21 11:25:32 +0100

    Explanations

--- test.R
+++ test.R
@@ -1,2 +1,7 @@
-# this is a test #
-# this is
+library(tidyverse)
+rne <- read_csv("Repertoire-national-des-elus.csv")
+mean(rne$Age[rne$Député == TRUE], na.rm = T)
+#the pipe (ctrl+shift+m) to create pipelines
+# functions for tidying data:
+# filter(), slice(), arrange(), select(), rename(), distinct(), mutate(), group_by(), summarise(), sample_n / sample_frac()
+  
