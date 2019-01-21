37f77de139482abaf61d71f49490d20096971ae2
Jules Beley <jules.beley@gmail.com>
2019-01-21 11:45:34 +0100

    commit

--- test.R
+++ test.R
@@ -7,4 +7,10 @@ mean(rne$Age[rne$Député == TRUE], na.rm = T)
 # filter(), slice(), arrange(), select(), rename(), distinct(), mutate(), group_by(), summarise(), sample_n / sample_frac()
 summary(rne)
 rne %>% 
-  filter(`Code sexe` %in% "F") #only elected women %in% instead of == to get rid of NAs
+  filter(`Code sexe` %in% "F") #only elected women %in% instead of == to get rid of NAs 
+# pipe is particularly useful when using several filters:
+rne_female <- rne %>% 
+  # filter(`Code sexe` %in% "F") %>% 
+  mutate(`Code sexe` = recode(`Code sexe`, "F" = "Female", "M" = "Male")) %>% 
+#mutate because we want to modify a bariable, recode to recode it
+  View()
